50365f7134bc82a77785430a798723be1072e360
Azael <73515917+AzaelHD1@users.noreply.github.com>
2022-06-06 17:08:50 -0500

    creacion de tabla user_domicilio

---
+++ database/migrations/2022_06_06_200411_create_user_domicilio.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_domicilio', function (Blueprint $table) {
+            $table->id('user_id');
+            $table->text('domicilio');
+            $table->integer('numero_exterior');
+            $table->string('colonia');
+            $table->integer('cp');
+            $table->string('ciudad');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_domicilio');
+    }
+};
