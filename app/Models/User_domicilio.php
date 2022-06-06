c5a2a4fe1747f35b1cf27a1c084ab0e0b4769f48
Azael <73515917+AzaelHD1@users.noreply.github.com>
2022-06-06 17:03:34 -0500

    Modelo User_domicilio

---
+++ app/Models/User_domicilio.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class User_domicilio extends Model
+{
+    protected $table ="user_domicilio";
+    public $timestamps = false;
+    use HasFactory;
+}
