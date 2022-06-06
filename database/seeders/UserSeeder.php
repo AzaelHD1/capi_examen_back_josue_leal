5e11ef7974f1be382745b7165a72851fb8c4d660
Azael <73515917+AzaelHD1@users.noreply.github.com>
2022-06-06 17:09:48 -0500

    seeder de userseeder
    se incrusta el modelo user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //busca el modelo user
+        \App\Models\User::factory(100)->create();
+    }
+}
